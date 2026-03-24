0ca01cac08858246dc32f5302a72d532d9ad18ce
aron-alv <l2225010077@guasave.tecnm.mx>
2026-03-23 18:44:53 -0600

    Agregar archivos de proyecto.

---
+++ AppGimnasio/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AppGimnasio
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
